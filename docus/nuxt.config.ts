0c38b53ccd90a89bfbe050a282f28eff06b059fa
Michael Stanclift <mx@vmstan.com>
2023-12-15 12:14:03 -0600

    package updates

--- docus/nuxt.config.ts
+++ docus/nuxt.config.ts
@@ -1,3 +1,11 @@
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/docus'
+  // https://github.com/nuxt-themes/docus
+  extends: '@nuxt-themes/docus',
+  devtools: { enabled: true },
+
+  modules: [
+    // Remove it if you don't use Plausible analytics
+    // https://github.com/nuxt-modules/plausible
+    '@nuxtjs/plausible'
+  ]
 })
